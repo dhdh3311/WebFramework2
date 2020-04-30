84ebfffe5ccce45998bc570aba43c6509a6230f1
doooomdoom <kgsyhs0301@naver.com>
2020-05-01 04:57:15 +0900

    4-3 완료

--- eStore/src/main/java/kr/ac/hansung/cse/controller/AdminController.java
+++ eStore/src/main/java/kr/ac/hansung/cse/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -53,7 +54,16 @@ public class AdminController {
 			System.out.println("Adding Product cannot be done");
 		
 		return "redirect:/admin/productInventory";
+			
+	}
+	
+	@RequestMapping(value="/productInventory/deleteProduct/{id}", method=RequestMethod.GET)
+	public String deleteProduct(@PathVariable int id) {
 		
+		if(!productService.deleteProduct(id))
+			System.out.println("Deleting Product cannot be done");
+		
+		return "redirect:/admin/productInventory";
 		
 	}
 }
