4a49b1d50fe9472ed87e3656b8a1e41fc4a185f7
doooomdoom <kgsyhs0301@naver.com>
2020-05-01 04:33:58 +0900

    4-2 완료

--- eStore/src/main/java/kr/ac/hansung/cse/controller/AdminController.java
+++ eStore/src/main/java/kr/ac/hansung/cse/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.ac.hansung.cse.model.Product;
 import kr.ac.hansung.cse.service.ProductService;
@@ -29,5 +30,30 @@ public class AdminController {
 		model.addAttribute("products", products);
 
 		return"productInventory";
+	}
+	
+	@RequestMapping(value="/productInventory/addProduct", method=RequestMethod.GET)
+	public String addProduct(Model model) {
+		
+		Product product = new Product();
+		
+		product.setCategory("컴퓨터");
+		
+		model.addAttribute("product", product);
+		
+		return "addProduct";
+		
+	}
+	
+	// web form data -> object(filled with form data)
+	@RequestMapping(value="/productInventory/addProduct", method=RequestMethod.POST)
+	public String addProductPost(Product product) { // controller -> service -> dao
+		
+		if (!productService.addProduct(product))
+			System.out.println("Adding Product cannot be done");
+		
+		return "redirect:/admin/productInventory";
+		
+		
 	}
 }
