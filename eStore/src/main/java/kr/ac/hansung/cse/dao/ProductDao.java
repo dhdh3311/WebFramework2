b0cbb1db0221fcb13eba41837d450723bee89511
doooomdoom <kgsyhs0301@naver.com>
2020-05-01 13:16:05 +0900

    4-4 완료

--- eStore/src/main/java/kr/ac/hansung/cse/dao/ProductDao.java
+++ eStore/src/main/java/kr/ac/hansung/cse/dao/ProductDao.java
@@ -15,26 +15,25 @@ import kr.ac.hansung.cse.model.Product;
 
 @Repository
 public class ProductDao {
-	
+
 	private JdbcTemplate jdbcTemplate;
-	
+
 	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	
-	
-	public List<Product> getProducts(){
-		
+
+	public List<Product> getProducts() {
+
 		String sqlStatement = "select * from product";
-		
+
 		return jdbcTemplate.query(sqlStatement, new RowMapper<Product>() {
 
 			@Override
 			public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
-				
+
 				Product product = new Product();
-				
+
 				product.setId(rs.getInt("id"));
 				product.setName(rs.getString("name"));
 				product.setCategory(rs.getString("category"));
@@ -42,16 +41,65 @@ public class ProductDao {
 				product.setManufacturer(rs.getString("manufacturer"));
 				product.setUnitInStock(rs.getInt("unitInStock"));
 				product.setDescription(rs.getString("description"));
-			
+
 				return product;
 			}
-			
+
 		});
-		
+
 	}
 
-
 	public boolean addProduct(Product product) {
+
+		int id = product.getId();
+		String name = product.getName();
+		String category = product.getCategory();
+		int price = product.getPrice();
+		String manufacturer = product.getManufacturer();
+		int unitInStock = product.getUnitInStock();
+		String description = product.getDescription();
+
+		String sqlStatement = "insert into product (id, name, category, price, manufacturer, unitInStock, description) "
+				+ "values (?,?,?,?,?,?,?)";
+
+		return (jdbcTemplate.update(sqlStatement,
+				new Object[] { id, name, category, price, manufacturer, unitInStock, description }) == 1);
+	}
+
+	public boolean deleteProduct(int id) {
+
+		String sqlStatement = "delete from product where id=?";
+
+		return (jdbcTemplate.update(sqlStatement, new Object[] { id }) == 1);
+	}
+
+	public Product getProductById(int id) {
+
+		String sqlStatement = "select * from product where id=?";
+
+		return jdbcTemplate.queryForObject(sqlStatement, new Object[] {id}, new RowMapper<Product>() {
+
+			@Override
+			public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+				Product product = new Product();
+
+				product.setId(rs.getInt("id"));
+				product.setName(rs.getString("name"));
+				product.setCategory(rs.getString("category"));
+				product.setPrice(rs.getInt("price"));
+				product.setManufacturer(rs.getString("manufacturer"));
+				product.setUnitInStock(rs.getInt("unitInStock"));
+				product.setDescription(rs.getString("description"));
+
+				return product;
+			}
+
+		});
+
+	}
+
+	public boolean updateProduct(Product product) {
 		
 		int id = product.getId();
 		String name = product.getName();
@@ -60,18 +108,11 @@ public class ProductDao {
 		String manufacturer = product.getManufacturer();
 		int unitInStock = product.getUnitInStock();
 		String description = product.getDescription();
-		
-		String sqlStatement = "insert into product (id, name, category, price, manufacturer, unitInStock, description) "
-				+ "values (?,?,?,?,?,?,?)";
-		
-		return (jdbcTemplate.update(sqlStatement, new Object[] {id, name, category, price, manufacturer, unitInStock, description }) ==1);
-	}
 
+		String sqlStatement = "update product set name=?, category=?, price=?, manufacturer=?, unitInStock=?, description=? "
+				+ "where id=?";
 
-	public boolean deleteProduct(int id) {
-		
-		String sqlStatement = "delete from product where id=?";
-	
-		return (jdbcTemplate.update(sqlStatement, new Object[] {id}) ==1);
+		return (jdbcTemplate.update(sqlStatement,
+				new Object[] { name, category, price, manufacturer, unitInStock, description, id }) == 1);
 	}
 }
