059d2c9a9e6ed53559bc7005fc8a8056217b5599
doooomdoom <kgsyhs0301@naver.com>
2020-05-13 17:32:35 +0900

    5-3 완료

--- eStore/src/main/java/kr/ac/hansung/cse/controller/HomeController.java
+++ eStore/src/main/java/kr/ac/hansung/cse/controller/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -17,9 +19,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	// 아래 두문장은 동일 
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	// private static final Logger logger = LoggerFactory.getLogger(kr.ac.hansung.cse.controller.HomeController);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(HttpServletRequest request, Locale locale) {
+		
+		// trace -> debug -> info -> warn -> error
+		// debug 이하 4레벨만 출력  
+		
+		logger.info("trace level: Welcome home! The client locale is {}", locale);
+		
+		String url = request.getRequestURL().toString();
+		String clientIPaddress = request.getRemoteAddr();
+		
+		logger.info("Request URL: {}, Client IP: {}", url, clientIPaddress);
 		
 		return "home";
 	}
